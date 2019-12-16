ebbbbe85b0a22ea68de421c93647d9ed2617ef8e
Millette-G <millette.gm@gmail.com>
2019-12-15 21:59:41 -0600

    First three done

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  return {
+    id: id,
+    name: name,
+    email: email
+  }
 }
 
 /**
@@ -56,8 +60,9 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName({name}) {
+  this.name = name;
+  return 'Hello, my name is ' + name;
 }
 
 /**
@@ -73,8 +78,13 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson({name}) {
+  this.name = name;
+  let sum = (num1, num2) => {
+    return num1 + num2;
+    }
+
+  return 'Hello, my name is ' + name;
 }
 
 
