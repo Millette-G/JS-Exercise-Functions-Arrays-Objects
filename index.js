ae5bd50dff83a4371389da3833b4bccb67040e22
Millette-G <millette.gm@gmail.com>
2019-12-16 22:26:42 -0600

    14 : 10 will be back early tomorrow

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ function getCarInfoByIndex(inventory, index) {
 */
 function getLastCarInfo(inventory) {
   /* code here */
-  return `This is a ${inventory[inventory.length-1].car_make}, ${inventory[inventory.length-1].car_model}`;
+  return `This is a ${inventory[inventory.length-1].car_make}, ${inventory[inventory.length-1].car_model}`
 }
 
 /**
@@ -205,8 +205,13 @@ function sortCarInventory(inventory) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
+function getModelYears(inventory) {
   /* code here */
+  let years = [];
+  for(let i = 0; i < inventory.length; i++) {
+    years.push(inventory[i].car_year)
+  }
+  return years;
 }
 
 /**
@@ -221,8 +226,14 @@ function getModelYears(/* code here */) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
+function getOlderCars(inventory, year) {
   /* code here */
+  let older = [];
+  for(let i = 0; i < inventory.length; i++) {
+    older.sort(inventory[i].car_year)
+    if 
+  }
+  return older;
 }
 
 /**
